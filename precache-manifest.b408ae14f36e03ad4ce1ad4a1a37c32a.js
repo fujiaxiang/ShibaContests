e44ba0d15668d19d7452603ada2072f158c44726
Jiaxiang Fu <jiaxiang@Jiaxiangs-MacBook-Pro.local>
2019-02-15 17:48:09 +0800

    Updates

---
+++ precache-manifest.b408ae14f36e03ad4ce1ad4a1a37c32a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "44353eaad6caad23daae",
+    "url": "/ShibaContests/static/css/main.aceb43c8.chunk.css"
+  },
+  {
+    "revision": "44353eaad6caad23daae",
+    "url": "/ShibaContests/static/js/main.44353eaa.chunk.js"
+  },
+  {
+    "revision": "9493e3af8d62274e35cc",
+    "url": "/ShibaContests/static/js/runtime~main.9493e3af.js"
+  },
+  {
+    "revision": "d029ae215ab88445501e",
+    "url": "/ShibaContests/static/css/2.43f2da17.chunk.css"
+  },
+  {
+    "revision": "d029ae215ab88445501e",
+    "url": "/ShibaContests/static/js/2.d029ae21.chunk.js"
+  },
+  {
+    "revision": "cc185bac68d06901f34a2b7a7196a5ba",
+    "url": "/ShibaContests/static/media/background.cc185bac.jpg"
+  },
+  {
+    "revision": "e15ca4fbb1b7d617a14bc9dee0c2b9d7",
+    "url": "/ShibaContests/index.html"
+  }
+];
